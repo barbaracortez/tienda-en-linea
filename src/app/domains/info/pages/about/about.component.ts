80ce19a728f9b1f94d385d3b5883298ac9183c9d
barbaracortez <cortezbarbara.dev@gmail.com>
2025-11-12 19:10:58 -0300

    configuracion y creacion de enviroment

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -3,17 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
-
-import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
-
 @Component({
   selector: 'app-about',
-  imports: [
-    CommonModule,
-    CounterComponent,
-    WaveAudioComponent,
-    HighlightDirective,
-  ],
+  imports: [CommonModule, CounterComponent, HighlightDirective],
   templateUrl: './about.component.html',
 })
 export default class AboutComponent {
